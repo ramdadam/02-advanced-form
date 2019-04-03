98e97a6a76f3ad1e78f3eb1ac7ea1f37815514c1
Tobias Lehwalder <tobias.lehwalder@cosee.biz>
2019-04-03 18:17:09 +0200

    linting stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import SimpleForm from "./components/SimpleForm";
 
 class App extends Component {
   render() {
+    let name = "risclog";
+    let someStr = "hallo" + name + "!";
+
+    console.log("lint it" + someStr);
+
     return (
       <div className="App">
         <SimpleForm />
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
+/* eslint-disable */
+const proxy = require("http-proxy-middleware");
 
-module.exports = function (app) {
-
-    app.use(proxy('/api', { target: 'http://localhost:8082/' }));
+module.exports = function(app) {
+  app.use(proxy("/api", { target: "http://localhost:8082/" }));
 };
